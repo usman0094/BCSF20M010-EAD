54e31e3d3162d8df3a62fe3e1cb7391408521956
unknown <osmanriaz094@gmail.com>
2023-10-01 20:56:48 +0500

    comments removed

--- EAD Assigmnet 01/Passenger.cs
+++ EAD Assigmnet 01/Passenger.cs
@@ -1,44 +1,4 @@
 ﻿
-
-//using System;
-//using System.Collections.Generic;
-
-//namespace EAD_Assignment01
-//{
-//	class Passenger
-//	{
-//		public string Name { get; set; }
-//		public string PhoneNo { get; set; }
-
-//		public Passenger(string name, string phoneNo)
-//		{
-//			Name = name;
-//			PhoneNo = phoneNo;
-//		}
-
-//		public int GiveRating()
-//		{
-//			Console.Write("\nGive Rating to this Ride out of 5: ");
-//			Console.ForegroundColor = ConsoleColor.Green;
-//			string ratingInput = Console.ReadLine();
-//			Console.ResetColor();
-
-//			int rating;
-//			while (!int.TryParse(ratingInput, out rating) || rating < 1 || rating > 5)
-//			{
-//				Console.ForegroundColor = ConsoleColor.Green;
-//				Console.Write("Invalid input. Please Enter a number between 1 and 5: ");
-//				ratingInput = Console.ReadLine();
-//				Console.ResetColor();
-//			}
-
-//			return rating;
-//		}
-//	}
-//}
-
-
-
 using System;
 using System.Collections.Generic;
 
